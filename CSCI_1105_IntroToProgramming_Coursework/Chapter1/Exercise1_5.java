30a70cf63022e72c503119f228f1e1052cff7a04
JRDowse <66639538+JRDowse@users.noreply.github.com>
2020-06-09 12:09:43 -0600

    Add files via upload

---
+++ CSCI_1105_IntroToProgramming_Coursework/Chapter1/Exercise1_5.java
@@ -0,0 +1,12 @@
+/*
+Jackson Dowse
+June 9th, 2020
+
+This program outputs the answer to a simple equation
+*/
+
+class Exercise1_5 {
+	public static void main(String[] args) {
+		System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+	}
+}
