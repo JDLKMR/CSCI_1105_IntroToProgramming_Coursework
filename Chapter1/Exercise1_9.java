3cc6a5be2f68015ab8fb0091d4bc8fd8427a47b3
JRDowse <66639538+JRDowse@users.noreply.github.com>
2020-06-09 12:45:12 -0600

    Add files via upload

---
+++ Chapter1/Exercise1_9.java
@@ -0,0 +1,13 @@
+/*
+Jackson Dowse
+June 9th, 2020
+
+This program displays the area and perimeter of a rectangle with a width of 4.5 and a height of 7.9
+*/
+
+class Exercise1_9 {
+	public static void main(String[] args) {
+		System.out.println(4.5 * 7.9);
+		System.out.println((4.5 * 2) + (7.9 * 2));
+	}
+}
