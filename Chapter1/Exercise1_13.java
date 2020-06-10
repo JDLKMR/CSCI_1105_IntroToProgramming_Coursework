d8b1cdc0cda517f016a91febf4cfb893330cedff
JRDowse <66639538+JRDowse@users.noreply.github.com>
2020-06-10 12:04:27 -0600

    Add files via upload

---
+++ Chapter1/Exercise1_13.java
@@ -0,0 +1,13 @@
+/*
+Jackson Dowse
+June 10th, 2020
+
+This program outputs the value of x and y after solving an equation
+*/
+
+class Exercise1_13 {
+	public static void main(String[] args) {
+		System.out.println((44.5 * 0.55 - 50.2 * 5.9) / (3.4 * 0.55 - 50.2 * 2.1));
+		System.out.println((3.4 * 5.9 - 44.5 * 2.1) / (3.4 * 0.55 - 50.2 * 2.1));
+	}
+}
